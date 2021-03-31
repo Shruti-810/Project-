f3107a237d058a930486118c6d074b1b5986468e
Shruti Makwana <shrutimak810@gmail.com>
2021-03-31 09:55:27 +0530

    Commit on March 31

--- Payment/views.py
+++ Payment/views.py
@@ -1,3 +1,13 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render
 
 # Create your views here.
+=======
+from django.shortcuts import render,redirect
+
+def payment(request):
+    if request.method=='POST':
+        
+    else:
+        return render(request,'payment.html')
+>>>>>>> Stashed changes
